699e4feafcb408bae732a873044db3fa3e043235
iannicelli <chiara.iannicelli30@gmail.com>
2023-07-12 17:01:51 +0200

    First steps MMU_writeByte

--- my_MMU.c
+++ my_MMU.c
@@ -15,6 +15,11 @@
 #define FLAG_UNSWAPPABLE (1 << 1)                // la pagina non può essere swappata
 #define FLAG_READ_BIT (1 << 2)                   // la pagina è stata letta 
 #define FLAG_WRITE_BIT (1 << 3)                  // la pagina è stata modificata 
+//aggiungo alcune flag utili
+#define FLAG_SECOND_CHANCE_BIT (1 << 4)          // riconoscere la pagina non acceduta da più tempo 
+#define FLAG_RESERVED (1 << 5)                   // pagina non modificabile (le prime due sono riservate alla taballea delle pagine)
+#define FLAG_CREATED (1 << 6)                    // pagina creata
+
 
 
 typedef struct
@@ -56,15 +61,38 @@ void MMU_init(MMU *mmu, char *file_name)
         mmu->page_table[i].frame_number = 0;
     }
 
-    mmu->page_table[0].flags = FLAG_UNSWAPPABLE | FLAG_VALID ;
+    mmu->page_table[0].flags = FLAG_UNSWAPPABLE | FLAG_VALID | FLAG_RESERVED | FLAG_CREATED;
     mmu->page_table[0].frame_number = 0;
 
-    mmu->page_table[1].flags = FLAG_UNSWAPPABLE | FLAG_VALID ;
+    mmu->page_table[1].flags = FLAG_UNSWAPPABLE | FLAG_VALID | FLAG_RESERVED | FLAG_CREATED;
     mmu->page_table[1].frame_number = 1;
 }
 
 void MMU_writeByte(MMU *mmu, int pos, char c)
 {
+    if (pos >= VIRTUAL_MEMORY_SIZE)
+    {
+        fprintf(stderr, "Cannot write to position %d, out of bounds\n", pos);
+        exit(1);
+    }
+
+    int page_number = pos / PAGE_SIZE;
+    //printf("Writing %c at position %d on page %d\n", c, pos, page_number);
+
+    // aggiungere controllo pagine riservate
+
+    if (mmu->page_table[page_number].flags & FLAG_VALID)
+    {
+       int frame_number = mmu->page_table[page_number].frame_number;
+
+        mmu->page_table[page_number].flags |= FLAG_WRITE_BIT;
+
+        mmu->physical_memory[frame_number * PAGE_SIZE + pos % PAGE_SIZE] = c;
+    }
+
+    else {
+        //printf("Page fault\n");
+    }
 
 }
 
@@ -79,12 +107,14 @@ void MMU_print_page_table(MMU *mmu)
     printf("Page table:\n");
     for (int i = 0; i < PAGE_TABLE_SIZE; i++)
     {
-        printf("%.4d) %c%c%c%c >\t%d\n", i,
+        printf("%.4d) %c%c%c%c >\t%d  '%c'\n", i,
                mmu->page_table[i].flags & FLAG_VALID ? 'V' : '-',
                mmu->page_table[i].flags & FLAG_UNSWAPPABLE ? 'U' : '-',
                mmu->page_table[i].flags & FLAG_READ_BIT ? 'R' : '-',
                mmu->page_table[i].flags & FLAG_WRITE_BIT ? 'W' : '-',
-               mmu->page_table[i].frame_number);
+               mmu->page_table[i].frame_number,
+               mmu->physical_memory[i]);
+               
     }
 }
 
@@ -93,8 +123,10 @@ void prova(){
     MMU mmu;
     MMU_init(&mmu, "prova.bin");
     
-    printf("%d\n", mmu.free_frame);
-    //fin qui tutto ok
+    for (size_t i = 2 ; i < VIRTUAL_MEMORY_SIZE; i++)
+    {
+        MMU_writeByte(&mmu, i, 'a');
+    }
 
     MMU_print_page_table(&mmu);
 
